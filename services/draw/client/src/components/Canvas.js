9fc3640e1a2d630a0800665742ef758cd76743e9
John Rapp <johnfarnes@hotmail.com>
2017-08-09 21:30:33 +0200

    Touch controls

--- services/draw/client/src/components/Canvas.js
+++ services/draw/client/src/components/Canvas.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TouchControls from './TouchControls';
 
 const CANVAS_WIDTH = 512;
 const CANVAS_HEIGHT = 512;
@@ -14,27 +15,46 @@ class Canvas extends Component {
   renderCanvas(canvas, points) {
     if (!canvas) { return; }
     const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    ctx.strokeStyle = "#df4b26";
+    ctx.lineJoin = "round";
+    ctx.lineWidth = 5;
 
     const drawPoint = ({ x, y }) => {
       const SQUARE_SIZE = 4;
       ctx.fillRect(x - SQUARE_SIZE / 2, y - SQUARE_SIZE / 2, SQUARE_SIZE, SQUARE_SIZE);
     };
 
-    points.forEach(drawPoint);
+    //points.forEach(drawPoint);
+    const first = path[0];
+    if (first) {
+      ctx.moveTo(first.x, first.y);
+      ctx.beginPath();
+      path.forEach(({x, y}) => ctx.lineTo(x,y));
+      ctx.stroke();
+    }
   }
 
   render() {
     const { points, onSetPoint } = this.props; 
     return (
-      <canvas
-        width={CANVAS_WIDTH}
-        height={CANVAS_HEIGHT}
-        ref={canvas => this.renderCanvas(canvas, points)}
-        style={{ border: '1px solid black' }}
-        onClick={e => { onSetPoint(canvasCoordinates(e)) }}
-      />
+      <TouchControls
+        onStartPath={point => {path = [point]; this.setState({date: Date.now()})}}
+        onAppendPath={point => {path.push(point); this.setState({date: Date.now()})}}
+        onEndPath={point => {path = []; this.setState({date: Date.now()})}}
+      >
+        <canvas
+          width={CANVAS_WIDTH}
+          height={CANVAS_HEIGHT}
+          ref={canvas => this.renderCanvas(canvas, points)}
+          style={{ border: '1px solid black' }}
+        />
+      </TouchControls>
     );
   }
 }
+
+let path = [];
 
 export default Canvas;
---
+++ services/draw/client/src/components/TouchControls.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+function relativeMouseCoordinates(event) {
+  const target = event.target;
+  const x = event.pageX - target.offsetLeft;
+  const y = event.pageY - target.offsetTop;
+  return { x, y };
+}
+
+function relativeTouchCoordinates(event) {
+  const target = event.target;
+  const x = event.touches[0].pageX - target.offsetLeft;
+  const y = event.touches[0].pageY - target.offsetTop;
+  return { x, y };
+}
+
+class TouchControls extends Component {
+  render() {
+    const { children, onStartPath, onAppendPath, onEndPath } = this.props; 
+    return (
+        <div
+            style={{ display: 'inline' }}
+            onMouseEnter={e => { this.mouseInside = true }}
+            onMouseLeave={e => {
+                this.mouseInside = false
+            //    onEndPath(relativeMouseCoordinates(e));
+            }}
+            onMouseDown={e => {
+                this.mouseDown = true;
+                onStartPath(relativeMouseCoordinates(e));
+            }}
+            onMouseUp={e => {
+                this.mouseDown = false;
+                onEndPath();
+            }}
+            onMouseMove={e => {
+                if (this.mouseDown) {
+                    onAppendPath(relativeMouseCoordinates(e));
+                }
+            }}
+            onTouchStart={e => {
+                this.mouseDown = true;
+                onStartPath(relativeTouchCoordinates(e));
+            }}
+            onTouchEnd={e => {
+                this.mouseDown = false;
+                onEndPath();
+            }}
+            onTouchMove={e => {
+                if (this.mouseDown) {
+                        onAppendPath(relativeTouchCoordinates(e));
+                    }
+                }}
+            >
+            { children }
+        </div>
+    );
+  }
+}
+
+export default TouchControls;
