c61b85a42901d30e8d1462ba28f5fd30cededccf
John Rapp <johnfarnes@hotmail.com>
2017-08-09 21:56:27 +0200

    test

--- services/draw/client/src/index.js
+++ services/draw/client/src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+try {
+
 ReactDOM.render(<App />, document.getElementById('root'));
+} catch(e) {
+    alert(e)
+}
 registerServiceWorker();
