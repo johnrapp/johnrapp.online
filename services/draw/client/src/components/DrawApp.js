68133def88f40f2100b39aab7044b99f06df88a9
John Rapp <johnfarnes@hotmail.com>
2017-08-12 23:04:55 +0200

    .length

--- services/draw/client/src/components/DrawApp.js
+++ services/draw/client/src/components/DrawApp.js
@@ -30,7 +30,7 @@ class DrawApp extends Component {
     if (!drawing) {
       return null;
     }
-    log(Object.entries(drawing.paths).length && Object.entries(drawing.paths)[0][1].points)
+    log(Object.entries(drawing.paths).length && Object.entries(drawing.paths)[0][1].points.length)
     return (
       <div className={'DrawApp'}>
         <div className={'draw-container'}>
