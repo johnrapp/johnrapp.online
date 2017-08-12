9e3d76bcee11b3d2e1bc2ef0052f05a6c5a6c138
John Rapp <johnfarnes@hotmail.com>
2017-08-12 22:14:06 +0200

    Okej, detta borde funka

--- services/draw/client/src/services/api.js
+++ services/draw/client/src/services/api.js
@@ -2,14 +2,13 @@ import axios from 'axios';
 
 const location = window.location;
 const HOST = ((location) => {
-    const isLocalDevelopment = host.port === '3000';
+    const isLocalDevelopment = location.port === '3000';
     if (isLocalDevelopment) {
         return `${location.hostname}:${8080}`;
     } else {
         return location.host;
     }
 })(location);
-
 
 export function getArchivedDrawings() {
     return axios({
--- services/draw/client/src/services/socket.js
+++ services/draw/client/src/services/socket.js
@@ -2,20 +2,13 @@ import io from 'socket.io-client';
 
 const location = window.location;
 const HOST = ((location) => {
-    const isLocalDevelopment = host.port === '3000';
+    const isLocalDevelopment = location.port === '3000';
     if (isLocalDevelopment) {
         return `${location.hostname}:${8080}`;
     } else {
         return location.host;
     }
 })(location);
-
-location.host === 'localhost:3000'
-    ? 'localhost:8080'
-    : location.host === '192.168.1.108:3000'
-        ? location.host === '192.168.1.108:3000'
-            ? '192.168.1.108:8080'
-            : '/';
 
 const socket = io(HOST);
 
