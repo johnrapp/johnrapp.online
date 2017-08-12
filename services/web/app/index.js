6f75ee12386a8e85299e3acaa79ba60ba87ebdbe
John Rapp <johnfarnes@hotmail.com>
2017-08-13 00:52:43 +0200

    Worked on web, built draw

--- services/web/app/index.js
+++ services/web/app/index.js
@@ -1,14 +1,44 @@
+const express = require('express');
 const path = require('path');
 const versions = require('./versions');
 
-const publicDir = path.join(__dirname, 'public');
+const rootDir = require('../root-dir');
+const publicDir = path.join(rootDir, 'public');
 
 module.exports = function(app) {
+    const router = express.Router({ strict: true });
+    bindRoutes(router);
+    app.use(router);
+}
+
+function bindRoutes(router) {
     app.get('/', (req, res) => {
-        res.json('hej!')
+        let { version = assignVersion(req) } = req.cookies;
+        sendVersion(version, res);
     });
 
     versions.forEach(version => {
-        
+        app.get(`/${version}`, (req, res) => {
+            sendVersion(version, res);
+        });
+        app.get(`/${version}`, (req, res) => {
+            sendVersion(version, res);
+        });
+        app.get(`/${version}/`, (req, res) => {
+            res.redirect(`/${version}`);
+        });
+        // app.get(`/${version}/**`, (req, res) => {
+        //     console.log('/version/**')
+        //     return res.send(req.url);
+        //     res.sendFile(path.join(publicDir, req.url));
+        // });
     });
+
+    function sendVersion(version, res) {
+        res.sendFile(path.join(publicDir, `${version}.html`));
+    }
+
+    function assignVersion(req) {
+        return 'shitty';
+    }
 }
--- services/web/index.js
+++ services/web/index.js
@@ -3,16 +3,19 @@ const morgan = require('morgan');
 const express = require('express');
 const app = express();
 const http = require('http');
+const cookieParser = require('cookie-parser');
 
 const abTestingApp = require('./app');
 
 const PORT = 80;
 
+app.use(cookieParser());
+
+abTestingApp(app);
+
 app.use(express.static('public'));
 
 app.use(morgan('tiny'));
-
-abTestingApp(app);
 
 const server = app.listen(PORT, function () {
   console.log(`Server listening on :${PORT}`);
