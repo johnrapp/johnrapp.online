3876834ee3303baa7915699193da09ebd53d7572
John Rapp <johnfarnes@hotmail.com>
2017-08-13 11:34:19 +0200

    Different determineVersion

--- services/web/app/assign-version.js
+++ services/web/app/assign-version.js
@@ -1,14 +1,17 @@
 const versionCookieOptions = {
-    expires: new Date(Date.now() + 1000 * 3600 * 24 * 30),
+    expires: false,
     httpOnly: true
 };
 module.exports = function assignVersion(req, res) {
-    const ip = req.ip;
-    const ipHash = hashInt(ip);
-    const version = versions[ipHash % versions.length];
+    const version = determineVersion(req);
 
-    //res.cookie('version', version, versionCookieOptions);
+    res.cookie('version', version, versionCookieOptions);
 
     console.log('Assigned', req.ip, 'to', version);
     return version;
+}
+
+function determineVersion(req) {
+    const index = Math.floor(Math.random() * versions.length);
+    return versions[index];
 }
