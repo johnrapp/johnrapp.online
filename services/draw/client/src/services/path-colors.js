4883ec3a48e9ec7a5e59edd87365726a6a340260
John Rapp <johnfarnes@hotmail.com>
2017-12-10 23:28:19 +0100

    Added mr clrs

--- services/draw/client/src/services/path-colors.js
+++ services/draw/client/src/services/path-colors.js
@@ -1,13 +1,18 @@
 const pathColors = [
-    '#000',
-    '#fff',
+    '#1A1617',
+    '#EEEBE6',
     '#3498db',
     '#2ecc71',
     '#f1c40f',
     '#e74c3c',
-    // Guaranteed
+    // Guaranteed realism
+    '#CA312B',
+    '#303030',
     '#E9A3A5',
-    '#B7447A'
+    '#B7447A',
+    '#563725',
+    '#C65E4A',
+    '#2E3D6A',
 ];
 
 export function defaultPathColor() {
